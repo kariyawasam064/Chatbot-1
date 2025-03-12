d26dd6018e482a22e81ac3c48582f702398bd973
Nadeesha Sewmini <nadeeshakarunasingha@gmail.com>
2025-03-12 10:04:41 +0530

    Update AppServiceProvider.php

--- New folder/app/Providers/AppServiceProvider.php
+++ New folder/app/Providers/AppServiceProvider.php
@@ -1,20 +1,31 @@
 <?php
-use Livewire\Livewire;
 
-public function boot(): void
+namespace App\Providers;
+use Carbon\Carbon;
+
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
 {
-    Carbon::setLocale(config('app.locale'));
-    date_default_timezone_set('Asia/Colombo'); // Set timezone globally
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
 
-    // Force HTTPS in production
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Carbon::setLocale(config('app.locale'));
+        date_default_timezone_set('Asia/Colombo'); // Set timezone globally
+
+          // Force HTTPS in production
     if ($this->app->environment('production')) {
         \URL::forceScheme('https');
     }
-
-    // Fix Livewire Redirect Issues
-    if ($this->app->environment('production')) {
-        Livewire::setUpdateRoute(function ($handle) {
-            return url()->to('/livewire/update', ['handle' => $handle]);
-        });
     }
 }
